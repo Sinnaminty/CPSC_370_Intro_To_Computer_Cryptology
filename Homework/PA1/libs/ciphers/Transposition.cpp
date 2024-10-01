2a7b98ee25c051d83fd15ad347f0d2327d094ac5
sin <aidenrivera7@gmail.com>
2024-09-30 21:14:41 -0400

    MEOW

--- Homework/PA1/libs/ciphers/Transposition.cpp
+++ Homework/PA1/libs/ciphers/Transposition.cpp
@@ -1,8 +1,9 @@
 #include "include/ciphers/Transposition.h"
 
-#include <algorithm>
-
 #include "include/ciphers/Utility.h"
 
 namespace Transposition {
+    Utility::CipherVector applyCipher ( const std::vector< int16_t > &vec,
+                                        const std::vector< int16_t > &key,
+                                        const Utility::OpType &opType ) {}
 }  // namespace Transposition
--- Homework/PA1/libs/ciphers/include/ciphers/Transposition.h
+++ Homework/PA1/libs/ciphers/include/ciphers/Transposition.h
@@ -3,14 +3,8 @@
 #include "Utility.h"
 
 namespace Transposition {
-    Utility::CipherVector du ( const std::vector< int16_t > &vec,
-                               const std::vector< int16_t > &key,
-                               const Utility::OpType &opType );
     Utility::CipherVector applyCipher ( const std::vector< int16_t > &vec,
                                         const std::vector< int16_t > &key,
                                         const Utility::OpType &opType );
-    std::vector< size_t > generateColumnOrder (
-        const std::vector< int16_t > &key );
-
 }  // namespace Transposition
 #endif
