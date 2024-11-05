045a89dc1a518a513000b4630d5c1ece3d1efe51
sin <aidenrivera7@gmail.com>
2024-11-04 23:35:24 -0500

    deleted the garbage, added cli utility

--- S-AES/app/main.cpp
+++ S-AES/app/main.cpp
@@ -2,26 +2,35 @@
 #include <cipher/SAES.h>
 #include <cipher/U.h>
 
+#include <cstring>
 #include <iostream>
-
+void printHelp ( ) {
+    std::cout << "-----------------------------------------------------\n";
+    std::cout << "S-AES IMPLEMENTATION\n";
+    std::cout << "By: Aiden Rivera\n";
+    std::cout << "-----------------------------------------------------\n";
+    std::cout << "USAGE: \n";
+    std::cout << "First arg is our input matrix, in bits or hex\n";
+    std::cout << "Second arg is our key matrix, in bits or hex\n";
+    std::cout << "Third arg is our operation, either E for encrypt or D for "
+                 "decrypt\n";
+}
 int main ( const int argc, const char *argv[] ) {
-    using namespace U;
-    using namespace SAES;
-
-    Matrix base = 0x6F6B;
-    Matrix difference = 0x6F9B;
-    int numPairs = 100;
-
-    auto pairs = DCA::generatePlaintextPairs ( base, difference, numPairs );
-    Matrix targetDiff = 0x00F0;  // This would be determined experimentally
-    Matrix keyGuess = DCA::differentialAttack ( pairs, targetDiff );
-
-    if ( ! ( keyGuess == Matrix ( 0xFFFF ) ) ) {
-        std::cout << "Key candidate found: " << keyGuess.toString ( ) << "\n";
-    } else {
-        std::cout << "No valid key found." << "\n";
+    if ( argc < 4 ) {
+        printHelp ( );
+        return 1;
     }
-
-    return 0;
+    const U::Matrix i ( argv[ 1 ] );
+    const U::Matrix k ( argv[ 2 ] );
+    U::Op op;
+    if ( std::strcmp ( argv[ 3 ], "E" ) ) {
+        op = U::Op::ENCRYPT;
+    } else if ( std::strcmp ( argv[ 3 ], "D" ) ) {
+        op = U::Op::DECRYPT;
+    } else {
+        std::cerr << argv[ 3 ] << " Not Valid operation!\n";
+        return 1;
+    }
+    std::cout << SAES::applyCipher ( i, k, op ).toString ( ) << "\n";
     return 0;
 }
--- S-AES/libs/cipher/include/cipher/DCA.h
+++ S-AES/libs/cipher/include/cipher/DCA.h
@@ -7,25 +7,6 @@
 #include "U.h"
 
 namespace DCA {
-    U::Matrix differentialAttack (
-        const std::vector< std::pair< U::Matrix, U::Matrix > > &pairs,
-        const U::Matrix &targetDiff );
-
-    std::vector< std::pair< U::Matrix, U::Matrix > > generatePlaintextPairs (
-        const U::Matrix &base,
-        const U::Matrix &diff,
-        const size_t &numPairs );
-
-    U::Matrix encryptToLastRound ( const U::Matrix &p, const U::Matrix &k );
-
-    U::Matrix calculateDifference ( const U::Matrix &c1, const U::Matrix &c2 );
-
-    std::vector< U::Matrix > getCiphertextDifferences (
-        const std::vector< std::pair< U::Matrix, U::Matrix > > &pairs,
-        const U::Matrix &key );
-
-    U::Matrix findMostCommonDifference (
-        const std::vector< U::Matrix > &diffs );
 
 };  // namespace DCA
 
